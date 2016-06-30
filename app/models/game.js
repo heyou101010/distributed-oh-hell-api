d73d7cc25f3f2fae311d480fda0c3022205b9d77
heyou101010 <csweikart@gmail.com>
2016-06-30 02:46:50 +0000

    adding getters and setters, and changing game model to be based on player array and making most getters be derivative from that.

--- app/models/game.js
+++ app/models/game.js
@@ -1,10 +1,24 @@
 export default class Game {
-  constructor(numberOfPlayers) {
-    this._numberOfPlayers = numberOfPlayers;
+  constructor(players) {
+    this._players = players;
   }
   
-  get topHand () {
+  get players() {
+    return this._players;
+  }
+  
+  set players(playerArray) {
+    this._players = playerArray;
+  }
+  
+  get numberOfPlayers() {
+    if (this._players) {
+      return this._players.length;
+    }
+  }
+  
+  get topHand() {
     let topHands = [0, 0, 0, 0, 10, 8, 7, 6, 5];
-    return topHands[this._numberOfPlayers];
+    return topHands[this._players.length];
   }
 }
