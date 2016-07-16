6984cb64a2c72b33fd3aae70b015cad48aa2bf90
heyou101010 <csweikart@gmail.com>
2016-07-16 17:22:50 +0000

    added getters and setter to User model

--- app/models/user.js
+++ app/models/user.js
@@ -5,5 +5,28 @@ export default class User {
       this._id = id;
     }
     
+    get firstName() {
+      return this._firstName;
+    }
+    
+    set firstName(userFirstName) {
+      this._firstName = userFirstName;
+    }
+    
+    get lastName() {
+      return this._lastName;
+    }
+    
+    set lastName(userLastName) {
+      this._lastName = userLastName;
+    }
+    
+    get id() {
+      return this._id;
+    }
+    
+    set id(userId) {
+      this._id = userId;
+    }
     
 }
